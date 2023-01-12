92882fe9498cd1bba61a2ac4a2ef1d89beacc4c9
Lailah Berry <lailahnicole614@gmail.com>
2023-01-12 13:08:38 -0800

    learned a string

--- test/tests.js
+++ test/tests.js
@@ -13,8 +13,14 @@ test('this test should pass', (expect) => {
 
 test('this test should add exclamation points', (expect) => {
     const expected = 'bunny rabbit!!!';
+    const expected2 = 'frogs!!!';
+    const expected3 = 'ducks!!!';
 
     const actual = addExclamationPoints('bunny rabbit');
+    const actual2 = addExclamationPoints('frogs');
+    const actual3 = addExclamationPoints('ducks');
 
     expect.deepEqual(actual, expected);
+    expect.deepEqual(actual2, expected2);
+    expect.deepEqual(actual3, expected3);
 });
